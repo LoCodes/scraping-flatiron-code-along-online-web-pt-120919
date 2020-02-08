322b0d704e7136b8cd909a7d61e20fca4771d42c
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-08 18:08:53 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,29 @@
+#Course
+#  instance methods
+    #title has a setter and a getter method for title
+    #schedule has a setter and a getter method for a course's schedule
+    #description has a setter and a getter method for a course's description
+
+#  class methods
+#    .all  returns an array of all the instances of the Course class
 
 
+class Course                                    #
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
+end
